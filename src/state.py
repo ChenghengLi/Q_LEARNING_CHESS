20ead7aa2fc50ce5f147976b6091226249ab3974
ChenghengLi <li471115381@gmail.com>
2022-11-22 03:30:42 +0100

    Refactoring and modify heuristics

---
+++ src/state.py
@@ -0,0 +1,15 @@
+import copy
+
+
+class State:
+    def __init__(self, stateW, stateB, parent, depth, player):
+        self.stateW = stateW.copy()
+        self.stateB = stateB.copy()
+        self.parent = parent
+        self.depth = depth
+        self.player = player
+        self.check = False
+
+
+    def __str__(self):
+        return "State: " + str(self.stateW) + " " + str(self.stateB)
